a67156363eb58f86112ef08b23a3f269e8e7222d
Vladislav <vladgrz40@gmail.com>
2020-02-04 21:11:29 +0300

    a lot of changes
    - updated StorageInterface lib
    - fixed ToolLib.breakCarriedTool function
    - fixed crop harvester textures
    - cropmatron can load items from hopper
    - moved some files

--- IndustrialCraft²/dev/machine/resource/сrop_matron.js
+++ IndustrialCraft²/dev/machine/resource/сrop_matron.js
@@ -1,11 +1,10 @@
 IDRegistry.genBlockID("cropMatron");
 Block.createBlock("cropMatron", [
-	{name: "Crop Matron", texture: [["machine_bottom", 0], ["cropmatron", 0],["cropmatron", 1],["cropmatron", 1],["cropmatron", 1],["cropmatron", 1]], inCreative: true}
+	{name: "Crop Matron", texture: [["machine_bottom", 0], ["cropmatron_top", 0], ["cropmatron_side", 0], ["cropmatron_side", 0], ["cropmatron_side", 0], ["cropmatron_side", 0]], inCreative: true}
 ], "opaque");
-
-TileRenderer.setStandartModel(BlockID.cropMatron, [["machine_bottom", 0], ["cropmatron", 0],["cropmatron", 1],["cropmatron", 1],["cropmatron", 1],["cropmatron", 1]], true);
-TileRenderer.registerRotationModel(BlockID.cropMatron, 0, [["machine_bottom", 0], ["cropmatron", 0],["cropmatron", 1],["cropmatron", 1],["cropmatron", 1],["cropmatron", 1]]);
-TileRenderer.registerRotationModel(BlockID.cropMatron, 4, [["machine_bottom", 0], ["cropmatron", 0],["miner_side", 1],["cropmatron", 2],["cropmatron", 3],["cropmatron", 3]]);
+TileRenderer.setStandartModel(BlockID.cropMatron, [["machine_bottom", 0], ["cropmatron_top", 0], ["cropmatron_side", 0], ["cropmatron_side", 0], ["cropmatron_side", 0], ["cropmatron_side", 0]], true);
+TileRenderer.registerRotationModel(BlockID.cropMatron, 0, [["machine_bottom", 0], ["cropmatron_top", 0], ["cropmatron_side", 0], ["cropmatron_side", 0], ["cropmatron_side", 0], ["cropmatron_side", 0]]);
+TileRenderer.registerRotationModel(BlockID.cropMatron, 4, [["machine_bottom", 0], ["cropmatron_top", 0], ["cropmatron_side", 3], ["cropmatron_side", 1], ["cropmatron_side", 2], ["cropmatron_side", 2]]);
 
 ItemName.addTierTooltip("cropMatron", 1);
 
@@ -15,19 +14,20 @@ Block.registerDropFunction("cropMatron", function(coords, blockID, blockData, le
 
 Callback.addCallback("PreLoaded", function(){
     Recipes.addShaped({id: BlockID.cropMatron, count: 1, data: 0}, [
-        "zxz",
-        "asa",
-        "www"
-    ], ['z', ItemID.circuitBasic, 0, 'x', 54, 0, 'a', ItemID.cellEmpty, 0, 's',  BlockID.machineBlockBasic, 0,'w',  ItemID.perches, 0]);
+        "cxc",
+        "a#a",
+        "nnn"
+    ], ['#', BlockID.machineBlockBasic, 0, 'x', 54, 0, 'c', ItemID.circuitBasic, 0, 'a', ItemID.cellEmpty, 0, 'n', ItemID.perches, 0]);
 });
+
 function isFertilizer(id){
-    return id == ItemID.fertilizer
+    return id == ItemID.fertilizer;
 };
 function isWeedEx(id){
-    return id == ItemID.weedEx
+    return id == ItemID.weedEx;
 };
 function isWater(id){
-    return id == ItemID.hydrationCell
+    return id == ItemID.hydrationCell;
 };
 var guiCropMatron = new UI.StandartWindow({
     standart: {
@@ -40,48 +40,25 @@ var guiCropMatron = new UI.StandartWindow({
 		slot: "default_slot",
 		invSlot: "default_slot"
     },
-
+    
     drawing: [
         {type: "background", color: Color.parseColor("#b3b3b3")},
         {type: "bitmap", x: 350, y: 150, bitmap: "energy_small_background", scale: GUI_SCALE}
     ],
-
+    
     elements: {
         "energyScale": {type: "scale", x: 350, y: 150, direction: 1, value: 1, bitmap: "energy_small_scale", scale: GUI_SCALE},
-        "slotFertilizer0": {type: "slot", x: 441, y: 75, bitmap: "dust_slot", isValid:isFertilizer},
-        "slotFertilizer1": {type: "slot", x: 441, y:135, isValid:isFertilizer},
-        "slotFertilizer2": {type: "slot", x: 441, y:195, isValid:isFertilizer},
-        "slotWeedEx0": {type: "slot", x: 641, y:75, bitmap: "weedEx_slot", isValid:isWeedEx},
-        "slotWeedEx1": {type: "slot", x: 641, y:135, isValid:isWeedEx},
-        "slotWeedEx2": {type: "slot", x: 641, y:195, isValid:isWeedEx},
-        "slotWater0": {type: "slot", x: 541, y:75,  bitmap: "cap_slot",isValid:isWater},
-        "slotWater1": {type: "slot", x: 541, y:135, isValid:isWater},
-        "slotWater2": {type: "slot", x: 541, y:195, isValid:isWater},
+        "slotFertilizer0": {type: "slot", x: 441, y: 75, bitmap: "dust_slot", isValid: isFertilizer},
+        "slotFertilizer1": {type: "slot", x: 441, y: 135, isValid: isFertilizer},
+        "slotFertilizer2": {type: "slot", x: 441, y: 195, isValid: isFertilizer},
+        "slotWeedEx0": {type: "slot", x: 641, y: 75, bitmap: "weedEx_slot", isValid: isWeedEx},
+        "slotWeedEx1": {type: "slot", x: 641, y: 135, isValid: isWeedEx},
+        "slotWeedEx2": {type: "slot", x: 641, y: 195, isValid: isWeedEx},
+        "slotWater0": {type: "slot", x: 541, y: 75,  bitmap: "cap_slot",isValid: isWater},
+        "slotWater1": {type: "slot", x: 541, y: 135, isValid: isWater},
+        "slotWater2": {type: "slot", x: 541, y: 195, isValid: isWater},
     }
 });
-var matronStoreSlots = {};
-var slotTypes = ["slotFertilizer","slotWeedEx","slotWater"];
-for(var i in slotTypes){
-    var slotName = slotTypes[i];
-    for(var j = 0; j < 3; j++){
-        var validFunc = undefined;
-        switch(slotName) {
-            case "slotFertilizer" :
-                validFunc = isFertilizer;
-                break;
-            case "slotWeedEx" :
-                validFunc = isWeedEx;
-                break;
-            case "slotWater" :
-                validFunc = isWater;
-                break;
-        }
-        matronStoreSlots[slotName + j] = {
-            input: true,
-            isValid:validFunc
-        }
-    }
-}
 
 Callback.addCallback("LevelLoaded", function(){
     MachineRegistry.updateGuiHeader(guiCropMatron, "Crop Matron");
@@ -97,23 +74,23 @@ MachineRegistry.registerElectricMachine(BlockID.cropMatron, {
         scanY: -1,
         scanZ: -4
     },
-
+    
     getGuiScreen: function(){
         return guiCropMatron;
     },
-
+    
     getTier: function(){
         return this.data.power_tier;
     },
-
-
+    
     tick: function(){
-        let newActive = false;
+		StorageInterface.checkHoppers(this);
         if(this.data.energy >= 31){
             this.scan();
-            newActive = true;
-        }
-        this.setActive(newActive);
+            this.setActive(true);
+        } else {
+			this.setActive(false);
+		}
         var energyStorage = this.getEnergyStorage();
         this.container.setScale("energyScale", this.data.energy / energyStorage);
     },
@@ -159,21 +136,31 @@ MachineRegistry.registerElectricMachine(BlockID.cropMatron, {
     },
     getSlot: function(type){
         for(var i = 0; i < 3; i++){
-            var slott = this.container.getSlot(type + i);
-            if(slott.id) return slott;
+            var slot = this.container.getSlot(type + i);
+            if(slot.id) return slot;
         }
-        return null
+        return null;
     },
-
+    
     getEnergyStorage: function(){
         return this.data.energy_storage;
     },
-
+    
     renderModel: MachineRegistry.renderModelWithRotation,
     energyReceive: MachineRegistry.basicEnergyReceiveFunc
 });
 
 TileRenderer.setRotationPlaceFunction(BlockID.cropMatron);
 StorageInterface.createInterface(BlockID.cropMatron, {
-	slots: matronStoreSlots
+	slots: {
+		"slotFertilizer^0-2": {input: true, isValid: function(item){
+			return item.id == ItemID.fertilizer;
+		}},
+		"slotWeedEx^0-2": {input: true, isValid: function(item){
+			return item.id == ItemID.weedEx;
+		}},
+		"slotWater^0-2": {input: true, isValid: function(item){
+			return item.id == ItemID.hydrationCell;
+		}}
+	}
 });

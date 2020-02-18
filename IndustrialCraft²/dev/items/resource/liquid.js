4fa43781c6334f71ef009f643ad136a7bf71694b
Vladislav <vladgrz40@gmail.com>
2020-02-18 09:03:59 +0300

    added coolant fluid

--- IndustrialCraft²/dev/items/resource/liquid.js
+++ IndustrialCraft²/dev/items/resource/liquid.js
@@ -1,2 +1,3 @@
 LiquidRegistry.registerLiquid("biomass", "Biomass", ["liquid_biomass"]);
 LiquidRegistry.registerLiquid("biogas", "Biogas", ["liquid_biogas"]);
+LiquidRegistry.registerLiquid("coolant", "Coolant", ["liquid_coolant"]);

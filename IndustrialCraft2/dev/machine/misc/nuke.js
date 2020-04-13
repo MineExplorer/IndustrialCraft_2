864f214899dc5dcfa4b6a2e88de7ac9a81deabde
Vladislav <vladgrz40@gmail.com>
2020-04-13 18:45:59 +0300

    Update nuke.js

--- IndustrialCraft2/dev/machine/misc/nuke.js
+++ IndustrialCraft2/dev/machine/misc/nuke.js
@@ -29,46 +29,44 @@ MachineRegistry.registerPrototype(BlockID.nuke, {
 	
 	explode: function(radius){
 		let x = this.x, y = this.y, z = this.z;
-		//function explodeNuke(x, y, z, radius){
-	World.explode(x + 0.5, y + 0.5, z + 0.5, 1);
-	let entities = Entity.getAll();
-	let rad = radius * 1.5;
-	for(let i in entities){
-		let ent = entities[i];
-		let c = Entity.getPosition(ent);
-		let dx = Math.abs(x - c.x), dy = Math.abs(y - c.y), dz = Math.abs(z - c.z);
-		let dist = Math.sqrt(dx*dx + dy*dy + dz*dz);
-		if(dist <= rad){
-			let damage = Math.ceil(rad*rad * 25 / (dist*dist));
-			if(damage >= 100){
-				Entity.damageEntity(ent, damage);
-			} else {
-				Entity.damageEntity(ent, damage, 11);
+		World.explode(x + 0.5, y + 0.5, z + 0.5, 1);
+		let entities = Entity.getAll();
+		let rad = radius * 1.5;
+		for(let i in entities){
+			let ent = entities[i];
+			let c = Entity.getPosition(ent);
+			let dx = Math.abs(x - c.x), dy = Math.abs(y - c.y), dz = Math.abs(z - c.z);
+			let dist = Math.sqrt(dx*dx + dy*dy + dz*dz);
+			if(dist <= rad){
+				let damage = Math.ceil(rad*rad * 25 / (dist*dist));
+				if(damage >= 100){
+					Entity.damageEntity(ent, damage);
+				} else {
+					Entity.damageEntity(ent, damage, 11);
+				}
 			}
 		}
-	}
-	
-	let height = radius/2;
-	for(let xx = -radius; xx <= radius; xx++)
-	for(let yy = -height; yy <= height; yy++)
-	for(let zz = -radius; zz <= radius; zz++){
-		if(Math.sqrt(xx*xx + yy*yy*4 + zz*zz) <= radius){
-			let block = World.getBlock(x + xx, y + yy, z + zz);
-			if(Block.getExplosionResistance(block.id) < 10000){
-				World.setBlock(x + xx, y + yy, z + zz, 0);
-				if(Math.random() < 0.01){
-					let drop = getBlockDrop({x: x + xx, y: y + yy, z: z + zz}, block.id, block.data, 100);
-					if(drop)
-					for(let i in drop){
-						let item = drop[i];
-						World.drop(x + xx, y + yy, z + zz, item[0], item[1], item[2]);
+		
+		let height = radius/2;
+		for(let dx = -radius; dx <= radius; dx++)
+		for(let dy = -height; dy <= height; dy++)
+		for(let dz = -radius; dz <= radius; dz++){
+			if(Math.sqrt(dx*dx + dy*dy*4 + dz*dz) <= radius){
+				let block = World.getBlock(x + dx, y + dy, z + dz);
+				if(Block.getExplosionResistance(block.id) < 10000){
+					World.setBlock(x + dx, y + dy, z + dz, 0);
+					if(Math.random() < 0.01){
+						let drop = getBlockDrop({x: x + dx, y: y + dy, z: z + dz}, block.id, block.data, 100);
+						if(drop)
+						for(let i in drop){
+							let item = drop[i];
+							World.drop(x + dx, y + dy, z + dz, item[0], item[1], item[2]);
+						}
 					}
 				}
 			}
 		}
-	}
 
-		
 		let sound = SoundAPI.playSoundAt(this, "Tools/NukeExplosion.ogg", false, 128);
 		RadiationAPI.addRadiationSource(this.x + 0.5, this.y + 0.5, this.z + 0.5, radius * 2, 600);
 	},

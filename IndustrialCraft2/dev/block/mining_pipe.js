d83859ede0ce5fd0397a5f219503746f14651ae1
Nikolay Savenko <savenkons@yandex.ru>
2020-05-02 20:51:31 +0300

    mining pipe recipe fix

--- IndustrialCraft2/dev/block/mining_pipe.js
+++ IndustrialCraft2/dev/block/mining_pipe.js
@@ -12,5 +12,5 @@ Callback.addCallback("PreLoaded", function(){
 		"p p",
 		"p p",
 		"pxp",
-	], ['x', 54, 0, 'p', ItemID.plateIron, 0]);
+	], ['x', ItemID.treetap, 0, 'p', ItemID.plateIron, 0]);
 });

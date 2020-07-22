7e097d84dc4200a15a4888993ecdc72738caaa7f
Vladislav <vladgrz40@gmail.com>
2020-07-22 16:25:49 +0300

    updated TileRender lib

--- IndustrialCraft2/lib/TileRender.js
+++ IndustrialCraft2/lib/TileRender.js
@@ -1,6 +1,6 @@
 LIBRARY({
 	name: "TileRender",
-	version: 16,
+	version: 17,
 	shared: true,
 	api: "CoreEngine"
 });
@@ -11,54 +11,39 @@ var EntityGetPitch = ModAPI.requireGlobal("Entity.getPitch");
 var TileRenderer = {
 	data: {},
 	
-	setStandartModel: function(id, texture, rotation){
-		if(rotation){
-			var textures = [
-				[texture[0], texture[1], texture[2], texture[3], texture[4], texture[5]],
-				[texture[0], texture[1], texture[3], texture[2], texture[5], texture[4]],
-				[texture[0], texture[1], texture[5], texture[4], texture[2], texture[3]],
-				[texture[0], texture[1], texture[4], texture[5], texture[3], texture[2]]
-			]
-			for(var i = 0; i < 4; i++){
-				var render = new ICRender.Model();
-				var model = BlockRenderer.createTexturedBlock(textures[i]);
-				render.addEntry(model);
-				BlockRenderer.enableCoordMapping(id, i, render);
-			}
-		} else {
-			var render = new ICRender.Model();
-			var model = BlockRenderer.createTexturedBlock(texture);
-			render.addEntry(model);
-			BlockRenderer.enableCoordMapping(id, -1, render);
-		}
-	},
-	
-	registerRenderModel: function(id, data, texture){
+	setStandartModel: function(id, texture, data) {
 		var render = new ICRender.Model();
 		var model = BlockRenderer.createTexturedBlock(texture);
 		render.addEntry(model);
-		if(!this.data[id]) this.data[id] = {};
+		BlockRenderer.enableCoordMapping(id, data || 0, render);
+	},
+	
+	registerRenderModel: function(id, data, texture) {
+		var render = new ICRender.Model();
+		var model = BlockRenderer.createTexturedBlock(texture);
+		render.addEntry(model);
+		this.data[id] = this.data[id] || {};
 		this.data[id][data] = render;
 	},
 	
-	registerRotationModel: function(id, data, texture){
+	registerRotationModel: function(id, data, texture) {
 		var textures = [
 			[texture[0], texture[1], texture[3], texture[2], texture[5], texture[4]],
 			[texture[0], texture[1], texture[2], texture[3], texture[4], texture[5]],
 			[texture[0], texture[1], texture[4], texture[5], texture[3], texture[2]],
 			[texture[0], texture[1], texture[5], texture[4], texture[2], texture[3]]
 		]
-		for(var i = 0; i < 4; i++){
+		for(var i = 0; i < 4; i++) {
 			this.registerRenderModel(id, data + i, textures[i]);
 		}
 	},
 	
-	registerFullRotationModel: function(id, data, texture){
-		if(texture.length == 2){
-			for(var i = 0; i < 6; i++){
+	registerFullRotationModel: function(id, data, texture) {
+		if (texture.length == 2) {
+			for(var i = 0; i < 6; i++) {
 				var textures = [];
-				for(var j = 0; j < 6; j++){
-					if(j == i) textures.push(texture[1]);
+				for(var j = 0; j < 6; j++) {
+					if (j == i) textures.push(texture[1]);
 					else textures.push(texture[0]);
 				}
 				this.registerRenderModel(id, i + data, textures);
@@ -72,42 +57,42 @@ var TileRenderer = {
 				[texture[0], texture[1], texture[4], texture[5], texture[3], texture[2]],
 				[texture[0], texture[1], texture[5], texture[4], texture[2], texture[3]],
 			]
-			for(var i = 0; i < 6; i++){
+			for(var i = 0; i < 6; i++) {
 				this.registerRenderModel(id, i + data, textures[i]);
 			}
 		}
 	},
 	
-	getRenderModel: function(id, data){
+	getRenderModel: function(id, data) {
 		var models = this.data[id];
-		if(models){
+		if (models) {
 			return models[data];
 		}
 		return 0;
 	},
 	
-	mapAtCoords: function(x, y, z, id, data){
+	mapAtCoords: function(x, y, z, id, data) {
 		var model = this.getRenderModel(id, data);
-		if(model){
+		if (model) {
 			BlockRenderer.mapAtCoords(x, y, z, model);
 		}
 	},
 	
-	getBlockRotation: function(isFull){
+	getBlockRotation: function(isFullRotation) {
 		var pitch = EntityGetPitch(Player.get());
-		if(isFull){
-			if(pitch < -45) return 0;
-			if(pitch > 45) return 1;
+		if (isFullRotation) {
+			if (pitch < -45) return 0;
+			if (pitch > 45) return 1;
 		}
 		var rotation = Math.floor((EntityGetYaw(Player.get())-45)%360 / 90);
-		if(rotation < 0) rotation += 4;
+		if (rotation < 0) rotation += 4;
 		rotation = [3, 1, 2, 0][rotation];
-		if(isFull) return rotation + 2;
+		if (isFullRotation) return rotation + 2;
 		return rotation;
 	},
 
-	setRotationPlaceFunction: function(id, fullRotation, placeSound){
-		Block.registerPlaceFunction(id, function(coords, item, block){
+	setRotationPlaceFunction: function(id, fullRotation, placeSound) {
+		Block.registerPlaceFunction(id, function(coords, item, block) {
 			var place = World.canTileBeReplaced(block.id, block.data) ? coords : coords.relative;
 			World.setBlock(place.x, place.y, place.z, item.id, 0);
 			World.playSound(place.x, place.y, place.z, placeSound || "dig.stone", 1, 0.8)
@@ -170,13 +155,13 @@ var TileRenderer = {
 	setPlantModel: function(){},
 	setCropModel: function(){},
 
-	setEmptyCollisionShape: function(id){
+	setEmptyCollisionShape: function(id) {
 		var shape = new ICRender.CollisionShape();
 		shape.addEntry().addBox(1, 1, 1, 0, 0, 0);
 		BlockRenderer.setCustomCollisionShape(id, -1, shape);
 	},
 	
-	getCropModel: function(texture){
+	getCropModel: function(texture) {
         var render = new ICRender.Model();
         var model = BlockRenderer.createModel();
         model.addBox(0.25, 0, 0, 0.25, 1, 1, texture[0], texture[1]);
@@ -187,23 +172,23 @@ var TileRenderer = {
         return render;
     },
 	
-	setSlabShape: function(id, count){
-		for(var i = 0; i < count; i++){
+	setSlabShape: function(id, count) {
+		for(var i = 0; i < count; i++) {
 			Block.setShape(id, 0, 0, 0, 1, 0.5, 1, i);
 		}
-		for(var i = count; i < count*2; i++){
+		for(var i = count; i < count*2; i++) {
 			Block.setShape(id, 0, 0.5, 0, 1, 1, 1, i);
 		}
 	},
 	
-	setSlabPlaceFunction: function(id, count, fullBlockID){
-		Block.registerPlaceFunction(id, function(coords, item, block){
-			if(block.id == item.id && block.data%count == item.data && parseInt(block.data/count) == (coords.side+1)%2){
+	setSlabPlaceFunction: function(id, count, fullBlockID) {
+		Block.registerPlaceFunction(id, function(coords, item, block) {
+			if (block.id == item.id && block.data%count == item.data && parseInt(block.data/count) == (coords.side+1)%2) {
 				World.setBlock(coords.x, coords.y, coords.z, fullBlockID, item.data);
 				return;
 			}
 			var place = World.canTileBeReplaced(block.id, block.data) ? coords : coords.relative;
-			if(coords.vec.y - place.y < 0.5){
+			if (coords.vec.y - place.y < 0.5) {
 				World.setBlock(place.x, place.y, place.z, item.id, item.data);
 			}
 			else {
@@ -212,6 +197,5 @@ var TileRenderer = {
 		});
 	}
 }
-
 
 EXPORT("TileRenderer", TileRenderer);

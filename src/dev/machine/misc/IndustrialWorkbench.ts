edd70ee428c01873ee6a6c25123296666a051a61
MineExplorer <vladgrz40@gmail.com>
2025-04-19 20:32:01 +0300

    provide correct playerUid for manual crafting

--- src/dev/machine/misc/IndustrialWorkbench.ts
+++ src/dev/machine/misc/IndustrialWorkbench.ts
@@ -108,12 +108,12 @@ namespace Machine {
             return false;
         }
 
-        provideRecipe(): ItemInstance {
+        provideRecipe(playerUid: number): ItemInstance {
             const recipe = Recipes.getRecipeByField(this.container);
             if (!recipe)
                 return null;
 
-            const result = Recipes.provideRecipeForPlayer(this.container, "", -1);
+            const result = Recipes.provideRecipeForPlayer(this.container, "", playerUid);
             if (result) {
                 this.refillItems();
                 this.container.setSlot("slotResult", 0, 0, 0);
@@ -149,9 +149,10 @@ namespace Machine {
         /** @deprecated Container event, shouldn't be called directly */
         @ContainerEvent(Side.Server)
         craftClick(packetData: any, client: NetworkClient) {
-            const result = this.provideRecipe();
+            const playerUid = client.getPlayerUid();
+            const result = this.provideRecipe(playerUid);
             if (result) {
-                new PlayerActor(client.getPlayerUid()).addItemToInventory(result.id, result.count, result.data, result.extra, true);
+                new PlayerActor(playerUid).addItemToInventory(result.id, result.count, result.data, result.extra, true);
             }
         }
 	}

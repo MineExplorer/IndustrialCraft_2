f47a9765fb048fe6b1465902fc84e844f577eb83
MineExplorer <vladgrz40@gmail.com>
2025-09-30 15:30:11 +0400

    reinforced door placing sound

--- src/dev/core/MathUtil.ts
+++ src/dev/core/MathUtil.ts
@@ -3,6 +3,10 @@ namespace MathUtil {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
+	export function randomFloat(min: number, max: number): number {
+		return Math.random() * (max - min) + min;
+	}
+
 	export function setInRange(value: number, minValue: number, maxValue: number) {
 		if (value < minValue) return minValue;
 		if (value > maxValue) return maxValue;
